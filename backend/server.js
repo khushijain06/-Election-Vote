1d799dd8eb0a12721ecb0cd2205e056b3a426879
khushijain06 <codingwithkhushi@gmail.com>
2025-07-17 16:19:26 +0530

    all

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const app = express();
 const db = require('./db');
+const cors = require('cors')
+const mongoose = require('mongoose')
 require('dotenv').config();
 
 const bodyParser = require('body-parser'); 
 app.use(bodyParser.json()); // req.body
 const PORT = process.env.PORT || 3000;
-// Import the router files
+
+app.use(cors())
 const userRoutes = require('./routers/users.routers');
 const candidateRoutes = require('./routers/candidate.routers');
 
-// Use the routers
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology :true
+}).then(()=> console.log("Mongo connected"))
+.catch(err => console.log(err));
+
 app.use('/user', userRoutes);
 app.use('/candidate', candidateRoutes);
 
